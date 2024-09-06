d247deb1e93c698bfc27e51331160989c2e60b96
Angel Perez <124532714+AngelcoreS@users.noreply.github.com>
2024-09-06 12:41:59 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Copy custom Nginx config to /etc/nginx
-cp /home/default /etc/nginx/sites-available/default
+cp /home/Web-App-Azure/default /etc/nginx/sites-available/default
 # Restart Nginx service
 service nginx restart
